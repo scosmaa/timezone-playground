f93d22d7cf6a6bb4c10b94117e43017b35db645c
scosmaa <scosmaa@gmail.com>
2017-02-17 16:05:00 +0100

    Created CET timezone examples

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-var jun = moment(new Date(1994, 8, 26));
-debugger;
-console.log(jun.tz('CET').format());
+var datetime = new Date(1994, 8, 26);
+var moment_wrapper = moment(datetime);
+var div = document.body.children[0];
+
+console.log("--- TZ String datetime ---");
+console.log(datetime.toString());
+addChild(div, "TZ String datetime:", datetime.toString());
+
+console.log("--- Iso String datetime ---");
+console.log(datetime.toISOString());
+addChild(div, "Iso String datetime:", datetime.toISOString());
+
+console.log("--- moment string ---");
+console.log(moment_wrapper.format());
+addChild(div, "moment string:", moment_wrapper.format());
+
+console.log("--- moment string using CET tz ---");
+console.log(moment_wrapper.tz('CET').format());
+addChild(div, "moment string using CET tz:", moment_wrapper.tz('CET').format());
+
+function addChild(div, title, value) {
+    var child = document.createElement("div");
+    child.innerHTML = ["<b>", title, "</b>", value].join('');
+    div.appendChild(child);
+}
